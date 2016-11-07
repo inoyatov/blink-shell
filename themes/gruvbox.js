8eed83414a4a0efd029ad850fbdf4bf8a9e11aa3
Khamidulla Inoyatov <khamidulla@inoyatov.com>
2016-11-07 17:50:31 +0900

    first commit

---
+++ themes/gruvbox.js
@@ -0,0 +1,23 @@
+solid_black		= '#1D2021';
+solid_red		= '#CC241D';
+solid_green		= '#98971A';
+solid_yellow	= '#D79921';
+solid_blue		= '#458588';
+solid_magenta	= '#B16286';
+solid_cyan		= '#689D6A';
+solid_white		= '#A89984';
+light_black		= '#928374';
+light_red		= '#FB4934';
+light_green		= '#B8BB26';
+light_yellow	= '#FABD2F';
+light_blue		= '#83A598';
+light_magenta	= '#D3869B';
+light_cyan		= '#8EC07C';
+light_white		= '#EBDBB2';
+
+t.prefs_.set('color-palette-overrides',
+				[solid_black, solid_red, solid_green, solid_yellow, solid_blue, solid_magenta, solid_cyan, solid_white, light_black, light_red, light_green, light_yellow, light_blue, light_magenta, light_cyan, light_white]);
+
+t.prefs_.set('cursor-color', '#BDAE93')
+t.prefs_.set('foreground-color', '#EBDBB2')
+t.prefs_.set('background-color', '#282828')
